5320a1f57d2a5bb5f3a00da752c4cd16d57a67cc
Wu Jane <jane72418@gmail.com>
2022-09-12 23:55:24 +0800

    adjust swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,15 +4,34 @@ var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
+
+  // centeredSlidesBounds: true,
+  autoplay: {
+    delay: 2000,
+  },
   slidesPerView: "auto",
   coverflowEffect: {
     rotate: 0,
-    stretch: 0,
+    stretch: -70,
     depth: 200,
     modifier: 1,
-    slideShadows: true,
+    slideShadows: false,
   },
   pagination: {
     el: ".swiper-pagination",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    375: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 2.8,
+    },
   },
 });
