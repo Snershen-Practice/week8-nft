9d7e39b72e2a9fecee7e293e36522417ca056f2a
Wu Jane <jane72418@gmail.com>
2022-09-14 17:37:03 +0800

    Update 2022-09-14T09:37:02.505Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -35,30 +35,5 @@ var swiper = new Swiper(".mySwiper", (_Swiper = {
   992: {
     slidesPerView: 2.83
   }
-}), _Swiper)); // var swiper = new Swiper(".series_Swiper", {
-//   // speed: 400,
-//   // spaceBetween: 100,
-//   slidesPerView: 3,
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     draggable: true,
-//   },
-//   breakpoints: {
-//     // when window width is >= 320px
-//     320: {
-//       slidesPerView: 1,
-//       // spaceBetween: 20,
-//     },
-//     // when window width is >= 480px
-//     480: {
-//       slidesPerView: 1,
-//       // spaceBetween: 30,
-//     },
-//     // when window width is >= 640px
-//     640: {
-//       slidesPerView: 4,
-//       // spaceBetween: 40,
-//     },
-//   },
-// });
+}), _Swiper));
 //# sourceMappingURL=all.js.map
